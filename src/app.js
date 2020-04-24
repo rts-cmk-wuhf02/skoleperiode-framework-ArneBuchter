161306867091688c770348e593889e9456f5c93a
Arne Emil Buchter <5651612@RTS-365.dk>
2020-04-24 12:09:01 +0200

    AddTools

--- src/app.js
+++ src/app.js
@@ -1,38 +1,35 @@
-    const Pet = ({ name, animal, breed}) =>{
-        return React.createElement("div", {}, [
-            React.createElement("h1", {}, name),
-            React.createElement("h2", {}, animal),
-            React.createElement("h2", {}, breed)
-        ]);
-    };
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
-        const App = () => {
-            return React.createElement(
-                "div",
-                {},
-                [
-                    React.createElement("h1", {}, "Adobt Me!"),
-                    React.createElement(Pet, {
-                        name: "Leo", 
-                        animal: "Rabbit", 
-                        breed: "Lionhead"
-                    }),
-                    React.createElement(Pet, {
-                        name: "Ramsey", 
-                        animal: "Dog", 
-                        breed: "Mix"
-                    }),
-                    React.createElement(Pet, {
-                        name: "Doink", 
-                        animal: "Rabbit", 
-                        breed: "Lionhead"
-                    })
-                
-                ]
-            );
-        }; 
+const App = () => {
+  /*   return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adobt Me!"),
+    React.createElement(Pet, {
+      name: "Leo",
+      animal: "Rabbit",
+      breed: "Lionhead",
+    }),
+    React.createElement(Pet, {
+      name: "Ramsey",
+      animal: "Dog",
+      breed: "Mix",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Rabbit",
+      breed: "Lionhead",
+    }),
+  ]); */
 
-        ReactDOM.render(
-            React.createElement(App),
-            document.getElementById("root")
-        );
+  return (
+    <div>
+      <h1>Adobt Me!</h1>
+      <Pet name="Leo" animal="Rabbit" breed="Lionhead" />
+      <Pet name="Ramsey" animal="Dog" breed="Mix" />
+      <Pet name="Doink" animal="Rabbit" breed="Lionhead" />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
