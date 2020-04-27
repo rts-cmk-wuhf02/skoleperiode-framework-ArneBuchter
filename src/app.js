8cdbb1c65c228abf188d3d22fc0f3b1d07c9ccfd
Arne Emil Buchter <5651612@RTS-365.dk>
2020-04-27 14:44:20 +0200

    AddJHooksStart

--- src/app.js
+++ src/app.js
@@ -1,33 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+/* import Pet from "./Pet"; */
+import SearchParams from "./searchparams";
+import SearhParams from "./searchparams";
 
 const App = () => {
-  /*   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adobt Me!"),
-    React.createElement(Pet, {
-      name: "Leo",
-      animal: "Rabbit",
-      breed: "Lionhead",
-    }),
-    React.createElement(Pet, {
-      name: "Ramsey",
-      animal: "Dog",
-      breed: "Mix",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Rabbit",
-      breed: "Lionhead",
-    }),
-  ]); */
-
   return (
     <div>
       <h1>Adobt Me!</h1>
-      <Pet name="Leo" animal="Rabbit" breed="Lionhead" />
-      <Pet name="Ramsey" animal="Dog" breed="Mix" />
-      <Pet name="Doink" animal="Rabbit" breed="Lionhead" />
+      <SearhParams />
     </div>
   );
 };
---
+++ src/searchparams.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const SearhParams = () => {
+  const [location, setLocation] = useState("Seatlle, WA");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearhParams;
