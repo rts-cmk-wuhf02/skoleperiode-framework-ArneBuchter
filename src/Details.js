e1da42cd9fe354dcb2712f51286c4a092f06d66c
Arne Emil Buchter <5651612@RTS-365.dk>
2020-05-07 13:16:34 +0200

    Addeee

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,8 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class details extends React.Component {
   state = { loading: true };
@@ -31,7 +33,9 @@ class details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adobt {name}</button>
+          <ThemeContext.Consumer>
+            {(themeHook) => <button>Adobt {name}</button>}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
